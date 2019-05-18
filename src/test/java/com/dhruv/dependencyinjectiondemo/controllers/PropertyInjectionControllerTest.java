db7683bdaf424703cea6299df58c9457f5a41f14
dhmaddy <dh.dhruv95@gmail.com>
2019-05-18 18:11:14 +0530

    Types of Dependency Injection Explained via Junit

--- src/test/java/com/dhruv/dependencyinjectiondemo/controllers/PropertyInjectionControllerTest.java
+++ src/test/java/com/dhruv/dependencyinjectiondemo/controllers/PropertyInjectionControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PropertyInjectedControllerTest {
+public class PropertyInjectionControllerTest {
 
     private PropertyInjectionController propertyInjectionController;
 
